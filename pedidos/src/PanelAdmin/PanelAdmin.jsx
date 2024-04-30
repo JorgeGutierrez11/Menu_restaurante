079efe61a50cf1dcecacb759cf79aa23d934c057
JorgeGutierrez11 <jorge1706gutierrez@gmail.com>
2024-04-30 00:49:20 -0500

    IndexPrincipal6

--- pedidos/src/PanelAdmin/PanelAdmin.jsx
+++ pedidos/src/PanelAdmin/PanelAdmin.jsx
@@ -1,33 +1,33 @@
 import "../Styles/PanelAdmin.css"
 import React, { useState } from 'react';
 import ListadoProductos from "./ListadoProductos";
- export function PanelAdmin(){
+export function PanelAdmin() {
 
 
-    return(
-    <div className="fondo">
-        <div className="contenedor">
-            <header className="header">
-            <h2>Panel de administración</h2>
-        </header>
+    return (
+        <div className="fondo">
+            <div className="contenedor">
+                <header className="header">
+                    <h2>Panel de administración</h2>
+                </header>
 
-        <main className="pedidos">
-            <h1>Pedidos</h1>
-            <p>
-                Administra las ordenes
-            </p>
-             <div> 
-            <h3><ListadoProductos/></h3>
-             </div>
+                <main className="pedidos">
+                    <h1>Pedidos</h1>
+                    <p>
+                        Administra las ordenes
+                    </p>
+                    <div>
+                        <h3><ListadoProductos /></h3>
+                    </div>
 
-        </main>
-    
-       
-        <footer className="footer">
+                </main>
 
-        </footer>
 
+                <footer className="footer">
+
+                </footer>
+
+            </div>
         </div>
-    </div>
     )
 }
